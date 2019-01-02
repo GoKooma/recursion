9f850e821b5ff6e4673124bf75d0295b2b907dd5
KweonMo Koo <mikekoo2010@gmail.com>
2019-01-02 08:09:31 -0800

    Initial commit -stringify in progress

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,53 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var str = "";
+  if (Array.isArray(obj) === true) {
+  	str = "[";
+  	if (obj.length > 0) {
+  		for (var i = 0; i < obj.length; i++) {
+	  		if (Array.isArray(obj[i])) {
+	  			str += stringifyJSON(obj[i]) + ",";
+	  		} else {
+	  			if (typeof obj[i] === "string") {
+		  			str += "\"" + obj[i] + "\",";
+		  		} else {
+		  			str = str + obj[i] + ",";
+		  		}
+	  		}
+	   	}
+
+	   	str = str.substr(0, str.length - 1) + "]";
+	} else {
+	  	str += "]";
+	}
+	// in the case of an object
+  } else if (obj instanceof Object) {
+  	str = "{";
+  	
+  	for (var i in obj) {
+  		if (i instanceof Object) {
+  			str += stringifyJSON(i) + ":";
+  		}
+  		if (typeof obj[i] === "string") {
+  			str = str + "\"" + i + "\":" + "\"" + obj[i] + "\"" + ",";
+  		} else if (obj[i] instanceof Object) {
+  			str += "\"" + i + "\":" + stringifyJSON(obj[i]) + ",";
+  		} else {
+  			str = str + "\"" + i + "\":" + obj[i] + ","
+  		}
+  	}
+  	if (str.length > 1) {
+  		str = str.substr(0, str.length - 1) + "}";
+  	} else {
+  		str += "}";
+  	}
+  	
+  } else if (typeof obj === "string") {
+  	str += "\"" + obj + "\"";
+  } else {
+  	str += obj;
+  }
+
+  return str;
 };
